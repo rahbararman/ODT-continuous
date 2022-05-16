5422af97a9cd8b336bf2eaadd9a1c8aab0bec9e5
rahbararman <rahbararman@gmail.com>
2022-05-16 16:40:22 +0200

    change job

--- jobEC2.sh
+++ jobEC2.sh
@@ -1,9 +1,8 @@
-/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset frog --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion EC2 --metric fscore --numrands 3
-/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset frog --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion IG --metric fscore --numrands 3
-/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset frog --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion US --metric fscore --numrands 3
-/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset frog --metric fscore --numrands 3 --alg efdt
-
-/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset breastcancer --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion EC2 --metric fscore --numrands 3
-/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset breastcancer --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion IG --metric fscore --numrands 3
-/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset breastcancer --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion US --metric fscore --numrands 3
+/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset frog --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion EC2 --metric fscore --numrands 3 &&
+/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset frog --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion IG --metric fscore --numrands 3 &&
+/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset frog --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion US --metric fscore --numrands 3 &&
+/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset frog --metric fscore --numrands 3 --alg efdt &&
+/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset breastcancer --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion EC2 --metric fscore --numrands 3 &&
+/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset breastcancer --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion IG --metric fscore --numrands 3 &&
+/opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset breastcancer --minhypo 500 --maxhypo 2500 --hypostep 500 --criterion US --metric fscore --numrands 3 &&
 /opt/anaconda3/bin/python3 /home/arman/continuous-exp/ODT-continuous/experiments.py --dataset breastcancer --metric fscore --numrands 3 --alg efdt
