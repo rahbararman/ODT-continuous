ceb5acbdf1014af70b818a48b6af22071f6d7340
rahbararman <rahbararman@gmail.com>
2022-08-23 17:05:22 +0200

    change features

--- experiments.py
+++ experiments.py
@@ -37,7 +37,7 @@ def main():
     alg = args.alg
     metric = args.metric
     #TODO: Hyperparameters for OFS
-    num_features_ofs = 12
+    num_features_ofs = 17
     eps_ofs = 0.1
     R_ofs = 10
     stepsize_ofs = 0.2
